d1c62fff23a6380299ef34331e0cc3969d8cf067
ferbin17 <ferbin17@gmail.com>
2019-10-21 17:57:33 +0530

    scope added

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -10,7 +10,9 @@ class Todo < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   scope :user_shared_todos, lambda { |current_user| Todo.joins(:shares).select("shares.*,todos.*").where("shares.user_id= ?", current_user.id).order(position: :desc) }
-  scope :user_shared_partial_todos, lambda { |active_status, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("todos.active=? and shares.user_id= ?", active_status, current_user.id).order(position: :desc) } 
+  scope :user_shared_partial_todos, lambda { |active_status, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("todos.active=? and shares.user_id= ?", active_status, current_user.id).order(position: :desc) }
+  scope :previous_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position < ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1) }
+  scope :next_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position > ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1) }
   scope :search, lambda { |like_keyword| where("body LIKE ?", like_keyword) }
   scope :active_only, ->  { where(active: true) }
   scope :inactive_only, -> { where(active: false) }
@@ -25,7 +27,7 @@ class Todo < ApplicationRecord
     @user = User.find(current_user.id)
     case direction
     when "down"
-      @nexttodo = Todo.joins(:shares).select("shares.*,todos.*").where("position < ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1)
+      @nexttodo = Todo.previous_todo(current_todo, current_user)
 
       current_todo = current_todo.shares.where(user_id: current_user.id)
       @nexttodo = @nexttodo[0].shares.where(user_id: current_user.id)
@@ -34,7 +36,7 @@ class Todo < ApplicationRecord
       @nexttodo[0].update(position: current_todo[0].position)
       current_todo[0].update(position: position)
     when "up"
-      @nexttodo = Todo.joins(:shares).select("shares.*,todos.*").where("position > ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1)
+      @nexttodo = Todo.next_todo(current_todo, current_user)
 
       current_todo = current_todo.shares.where(user_id: current_user.id)
       @nexttodo = @nexttodo[0].shares.where(user_id: current_user.id)
