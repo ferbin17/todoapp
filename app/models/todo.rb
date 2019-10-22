10a6c3f55dc63145947f43893db7da7f1f88d4ea
ferbin17 <ferbin17@gmail.com>
2019-10-22 12:01:42 +0530

    search in show shortened

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -11,8 +11,8 @@ class Todo < ApplicationRecord
 
   scope :user_shared_todos, lambda { |current_user| Todo.joins(:shares).select("shares.*,todos.*").where("shares.user_id= ?", current_user.id).order(position: :desc) }
   scope :user_shared_partial_todos, lambda { |active_status, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("todos.active=? and shares.user_id= ?", active_status, current_user.id).order(position: :desc) }
-  scope :previous_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position < ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1) }
-  scope :next_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position > ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(:position).limit(1) }
+  scope :previous_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position < ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(position: :desc).limit(1) }
+  scope :next_todo, lambda { |current_todo, current_user| Todo.joins(:shares).select("shares.*,todos.*").where("position > ? and shares.user_id= ? and todos.active=?", current_todo.position, current_user.id, current_todo.active?).order(position: :asc).limit(1) }
   scope :search, lambda { |like_keyword| where("body LIKE ?", like_keyword) }
   scope :active_only, ->  { where(active: true) }
   scope :inactive_only, -> { where(active: false) }
