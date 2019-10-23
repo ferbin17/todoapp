5eb615556f1641dc4a7699ccad3fd135458718d0
ferbin17 <ferbin17@gmail.com>
2019-10-23 18:22:57 +0530

    respond_to :js removed from share controller

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,12 +1,11 @@
 class SharesController < ApplicationController
 
-  respond_to :html, :js
+  respond_to :js
 
   #shows all the users with check for shared users on share button click
   def index
     @users =  User.all.all_user_except_one(current_user)
     @shares = Share.select_user_id.shared_users(params[:id])
-    respond_to :js
   end
 
   #add or remove new users as shared users for a particular todo
